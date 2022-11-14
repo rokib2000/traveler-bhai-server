6e9a91ad63277ce69e7f266997b1041b7aca9ec7
rokib2000 <rokib0w1@gmail.com>
2022-11-15 02:34:14 +0600

    show single service

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { json } = require("express");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,13 +25,21 @@ async function run() {
   try {
     const serviceCollection = client.db("travelerBhai").collection("services");
 
-    //   show services client side
+    //   get services client side
     app.get("/services", async (req, res) => {
       const query = {};
       const sort = { date: -1 };
       const cursor = serviceCollection.find(query).sort(sort);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    // get single post data
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
 
     //   Add service

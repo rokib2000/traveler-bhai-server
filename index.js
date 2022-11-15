0783d7352b4508e9fc3981a42c3b75f968debb7b
rokib2000 <rokib0w1@gmail.com>
2022-11-15 06:55:18 +0600

    review delete

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ async function run() {
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
+
+    //review delete
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     //
   }
